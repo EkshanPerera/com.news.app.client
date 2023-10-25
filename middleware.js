000471f36d23270a7172e8ebf77317bc8dfb535c
Ekshan Perera <ekshan.perera@gmail.com>
2023-10-25 08:31:16 +0530

    V 1.7

--- middleware.js
+++ middleware.js
@@ -4,10 +4,10 @@ export default function middleware(req){
     let verify = req.cookies.get("loggedin");
     let url = req.url;
 
-    if(!verify && url.includes(`/dashboard`)){
+    if(!verify && url.includes(`http://localhost:3000/dashboard`)){
         return NextResponse.redirect(`http://localhost:3000/login`);
     }
-    if(!verify && url.includes(`/dashboard`)){
+    if(!verify && url.includes(`https://com-news-app-client.vercel.app/dashboard`)){
         return NextResponse.redirect(`https://com-news-app-client.vercel.app/login`);
     }
     if (verify && url === `http://localhost:3000/login`) {
